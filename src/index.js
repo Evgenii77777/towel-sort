9a6595ff5e7bdb641fa04880f2cb417ac6ffc310
Evgenii77777 <76007615+Evgenii77777@users.noreply.github.com>
2022-01-26 16:42:42 +0300

    add all file to indexjs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+  
+  newMatrix = [];
+  if (matrix=== undefined || matrix.length === 0) {
+    return newMatrix
+  }
+  matrix.forEach((el, index) => {
+    if (index % 2 === 0) {
+      el.sort((a,b)=>a-b);
+    } else {
+      el.sort((a, b) => b - a);
+    }
+    el.forEach((item) => {
+      newMatrix.push(item);
+    });
+  });
+  return newMatrix;
 }
